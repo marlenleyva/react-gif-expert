4b05c094829025d1b74ce07b66e649576a2a3ee6
Marlen <marlenleyvaa@gmail.com>
2024-12-23 16:23:22 -0300

    Primer commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,18 @@
+
+export const getGifs = async( category ) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=u5BQ5kjq6S94uyYFBft5HJ3LJEQWvE9a&q=${ category }&limit=10`;
+
+    const resp = await  fetch( url );
+
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => ( {
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    } ) );
+
+    return gifs;
+
+}
